9d68ef9c52ed5fd34571f868ff764f03f08eb197
Gaelan D'costa <gdcosta@gmail.com>
2021-01-30 19:47:08 -0500

    Empty elisp file

---
+++ fez-scheme.el
@@ -0,0 +1,19 @@
+;;; fez-scheme.el --- Trivial Scheme implemented in Elisp
+
+;; Copyright (C) 2021 Gaelan D'costa
+
+;; Author: Gaelan D'cost <gdcosta@gmail.com>
+;; Version: 0.0.1
+;; Keywords: scheme
+;; URL: https://github.com/RobotDisco/fez-scheme
+
+;;; Commentary:
+
+;; This package contains my attempt to implement the exercises in
+;; Christian Quinnec's textbook "Lisp in Small Pieces.", implemented
+;; in the Emacs Lisp runtime.…
+
+;;; Code:
+
+(provide 'fez-scheme)
+;;; fez-scheme.el ends here
